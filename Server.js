fe5ccbbe89fc3c2b9277ffb07a516be81e94a0b2
Mess-Dev <51171387+MesshypeWeb@users.noreply.github.com>
2020-07-26 21:11:53 +0200

    Added Qrcode output for scanning in app

--- Server.js
+++ Server.js
@@ -2,6 +2,17 @@ var express = require("express")
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var qrcode = require("qrcode-terminal")
+
+var address, os = require('os'), ifaces = os.networkInterfaces();
+
+for (var dev in ifaces) {
+    var iface = ifaces[dev].filter(function (details) {
+        return details.family === 'IPv4' && details.internal === false;
+    });
+    if (iface.length > 0) address = iface[0].address;
+}
+
 
 app.use("/public", express.static("public"));
 
@@ -9,51 +20,53 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/lib/demo.html")
 });
 
-app.get("/pointer", (req, res) =>{
+app.get("/pointer", (req, res) => {
     res.sendFile(__dirname + "/lib/Pointer.html")
 })
 var usersonline = 0;
-io.on("connection", (socket) =>{
-        usersonline++;
-   
+io.on("connection", (socket) => {
+    usersonline++;
+
     console.log(socket.id)
     console.log(`1 user connected. Currently ${usersonline} Users are online`);
-    socket.on("height", (numbers) =>{
+    socket.on("height", (numbers) => {
         socket.broadcast.emit("height", numbers);
     });
 
-    socket.on("Coordinates", (coordinates) =>{
-               socket.broadcast.emit("coordinates", coordinates)
+    socket.on("Coordinates", (coordinates) => {
+        socket.broadcast.emit("coordinates", coordinates)
     });
-    socket.on("ChangeSlide", (msg) =>{
-            socket.broadcast.emit('ChangeSlide', msg);
-        
+    socket.on("ChangeSlide", (msg) => {
+        socket.broadcast.emit('ChangeSlide', msg);
+
     })
-    socket.on("currentSlide", (msg) =>{
-          socket.broadcast.emit('Slide', msg)
+    socket.on("currentSlide", (msg) => {
+        socket.broadcast.emit('Slide', msg)
     });
-    socket.on("jumpto", (slide) =>{
-            socket.broadcast.emit("jumpto", {
-                slide: slide,
-                n: false
-            })
-        
+    socket.on("jumpto", (slide) => {
+        socket.broadcast.emit("jumpto", {
+            slide: slide,
+            n: false
+        })
+
     });
-    socket.on("paused", type =>{
+    socket.on("paused", type => {
         socket.broadcast.emit("paused", type)
     })
-    socket.on("pause", (type) =>{
+    socket.on("pause", (type) => {
         console.log(type);
         socket.broadcast.emit("pause", type);
     })
-    
-    socket.on('disconnect', function(){
+
+    socket.on('disconnect', function () {
         usersonline--;
         console.log(`1 User disconnected. Currently ${usersonline} Users are online`)
     })
 });
 
 
-http.listen(3000, function(){
+http.listen(3000, function () {
+    qrcode.generate(`http://${address}:3000/pointer`)
     console.log('listening on *:3000');
-  });
+});
+
--- public/js/Pointer.js
+++ public/js/Pointer.js
@@ -1,138 +1,113 @@
-if("undefined"==typeof jQuery)throw new Error("Pointer.js needs Jquery");
+if ("undefined" == typeof jQuery) throw new Error("Pointer.js requires Jquery");
 var connecto;
 var d = document;
-window.oncontextmenu = function(e){
+window.oncontextmenu = function (e) {
     e.preventDefault();
 }
 var z;
 
-function initialize(object){
-        connecto = object.server || undefined;
-        if(object.notes == false){
-            d.getElementById("notes").style.visibility = "hidden"
-        }else if(object.notes == true){
-            d.getElementById("notes").style.visibility = "visible"
-        }else if(typeof object.notes == "undefined"){
-            d.getElementById("notes").style.visibility = "visible"
-        }
-        var script = [], crS = d.createElement("script");
-        var style = [], crL = d.createElement("link");
-        var head = d.getElementsByTagName("head")[0];
-        if(object.dependencies){
-            for(let i = 0; i < object.dependencies.length; i++){
-                if(object.dependencies[i].type == "js"){
-                    crS.src = object.dependencies[i].src;
-                    if(object.dependencies[i].async == true){
-                        crS.attributes = ["async", "true"]
-                    }
-                    script.push(crS)
-                    crS = d.createElement("script");
-                }else if(object.dependencies[i].type == "css"){
-                    crL.href = object.dependencies[i].src;
-                    crL.rel = "stylesheet"
-                    style.push(crL);
-                    crL = d.createElement("link")
+function initialize(object) {
+    connecto = object.server || undefined;
+    if (object.notes == false) {
+        d.getElementById("notes").style.visibility = "hidden"
+    } else if (object.notes == true) {
+        d.getElementById("notes").style.visibility = "visible"
+    } else if (typeof object.notes == "undefined") {
+        d.getElementById("notes").style.visibility = "visible"
+    }
+    var script = [], crS = d.createElement("script");
+    var style = [], crL = d.createElement("link");
+    var head = d.getElementsByTagName("head")[0];
+    if (object.dependencies) {
+        for (let i = 0; i < object.dependencies.length; i++) {
+            if (object.dependencies[i].type == "js") {
+                crS.src = object.dependencies[i].src;
+                if (object.dependencies[i].async == true) {
+                    crS.attributes = ["async", "true"]
                 }
-            }
-            for(let i = 0; i < script.length; i++){
-                head.appendChild(script[i]);
-            }
-            for(let i = 0; i < style.length; i++){
-                head.appendChild(style[i]);
+                script.push(crS)
+                crS = d.createElement("script");
+            } else if (object.dependencies[i].type == "css") {
+                crL.href = object.dependencies[i].src;
+                crL.rel = "stylesheet"
+                style.push(crL);
+                crL = d.createElement("link")
             }
         }
+        for (let i = 0; i < script.length; i++) {
+            head.appendChild(script[i]);
+        }
+        for (let i = 0; i < style.length; i++) {
+            head.appendChild(style[i]);
+        }
+    }
 }
 var Pointer = {
     initialize: initialize
 }
-$(function(){
+$(function () {
     $("#pointer").hide();
 
     console.log(connecto)
     var socket = io();
-    
 
-    var nxtSlide = d.getElementById("nextSlide")
-    function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(d.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
+    $("#onlyPointer").click(function (e) {
+        $("#main").hide();
+        $("#pointer").show();
+        $("#hid").val(true)
+    })
+    $("#closePointer").click(function (a) {
+        $("#main").show();
+        $("#pointer").hide();
+        $("#hid").val(false);
+    })
+
+    window.ontouchmove = function (e, err) {
+        if ($("#hid").val() == "true") {
+
+            if (err) throw err;
+            var X = e.targetTouches[0].clientX;
+            var Y = e.targetTouches[0].clientY;
+            X = X / window.innerWidth;
+            Y = Y / window.innerHeight;
+            if (X && Y) {
+                socket.emit("Coordinates", {
+                    X: X,
+                    Y: Y,
+                    token: token
+                })
+            }
+
+            window.ontouchend = function (e) {
+                socket.emit("Coordinates", false)
+            }
         }
     }
-    return "";
-}
-if(getCookie("token") != ""){
-    var token = getCookie("token");
-}else{
-     var token = Math.random().toString(36).substr(2); // remove `0.`
-     d.cookie = `token=${token}`;
-     socket.emit("SetToken", token);
-};
+    socket.on('disconnect', function () {
+        console.log('disconnected')
+        d.cookie = "token=;";
+    })
 
-$("#onlyPointer").click(function(e){
-    $("#main").hide();
-    $("#pointer").show();
-    $("#hid").val(true)
-})
-$("#closePointer").click(function(a){
-    $("#main").show();
-    $("#pointer").hide();
-    $("#hid").val(false);
-})
-
-window.ontouchmove = function(e, err){
-    if($("#hid").val() == "true"){
-        
-        if(err) throw err;
-        var X = e.targetTouches[0].clientX;
-        var Y = e.targetTouches[0].clientY;
-        X = X / window.innerWidth;
-        Y = Y / window.innerHeight;
-        if(X && Y){
-            socket.emit("Coordinates", {
-                X: X,
-                Y: Y,
-                token: token
-            })
-        }
-    
-        window.ontouchend = function(e){
-            socket.emit("Coordinates", false)
-        }
-      }
+    d.getElementById("Up").onclick = function () {
+        socket.emit("ChangeSlide", "up")
     }
-socket.on('disconnect', function(){
-    console.log('disconnected')
-    d.cookie = "token=;";
-})
-
- d.getElementById("Up").onclick = function(){
-    socket.emit("ChangeSlide","up")
-}
-d.getElementById("left").onclick = function(){
-    socket.emit("ChangeSlide","left")
-}
-d.getElementById("right").onclick = function(){
-    socket.emit("ChangeSlide", "right")
-}
-d.getElementById("down").onclick = function(){
-    socket.emit("ChangeSlide", "down")
-}
-socket.on('Slide', function(num){
+    d.getElementById("left").onclick = function () {
+        socket.emit("ChangeSlide", "left")
+    }
+    d.getElementById("right").onclick = function () {
+        socket.emit("ChangeSlide", "right")
+    }
+    d.getElementById("down").onclick = function () {
+        socket.emit("ChangeSlide", "down")
+    }
+    socket.on('Slide', function (num) {
         d.getElementById("currentSlide").innerHTML = num.A;
-        if(num.B != null){
+        if (num.B != null) {
             $("#notes").html(num.B)
-        }else{
+        } else {
             $("#notes").html("")
         }
-       
+
     })
     /*
     d.getElementById("jumpForm").onsubmit = function(e){
@@ -142,55 +117,55 @@ socket.on('Slide', function(num){
         else alert("Please type in a number");
     }*/
     var ispaused = false;
-        $("#pause").click(function(){
-            if(ispaused){ $("#top").show(); $(this).html("Pause Presentation");socket.emit("pause", false); ispaused = false; return};
-            socket.emit("pause", true);
-            $("#top").hide();
-            $(this).html("Resume Presentation");
+    $("#pause").click(function () {
+        if (ispaused) { $("#top").show(); $(this).html("Pause Presentation"); socket.emit("pause", false); ispaused = false; return };
+        socket.emit("pause", true);
+        $("#top").hide();
+        $(this).html("Resume Presentation");
+        ispaused = true;
+    })
+    socket.on("paused", function (type) {
+        if (type == true) {
             ispaused = true;
-        })
-        socket.on("paused", function(type){
-            if(type == true){
-                ispaused = true;
-                $("#top").hide();
-                $("#pause").html("Resume Presentation");
-            }else if(type == false){
-                ispaused = false;
-                $("#top").show(); 
-                $("#pause").html("Pause Presentation");
-                return;
-            }
-        })
-        socket.on("pauses", function(type){
-            if(type == true){
-                ispaused = true;
-                $("#top").hide();
-                $("#pause").html("Resume Presentation");
-            }else if(type == false){
-                ispaused = false;
-                $("#top").show(); 
-                $("#pause").html("Pause Presentation");
-                return;
-            }
-        })
+            $("#top").hide();
+            $("#pause").html("Resume Presentation");
+        } else if (type == false) {
+            ispaused = false;
+            $("#top").show();
+            $("#pause").html("Pause Presentation");
+            return;
+        }
+    })
+    socket.on("pauses", function (type) {
+        if (type == true) {
+            ispaused = true;
+            $("#top").hide();
+            $("#pause").html("Resume Presentation");
+        } else if (type == false) {
+            ispaused = false;
+            $("#top").show();
+            $("#pause").html("Pause Presentation");
+            return;
+        }
+    })
 })
 var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close")[0];
-  $("#SlidePreview").click(function(e) {
-     
-        modal.style.display = "block";
-        link = this;
-      
-        var iframe = document.createElement("iframe");
-        iframe.src
-    });
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-    
-  
-    window.onclick = function(event) {
-      if (event.target == modal) {
+$("#SlidePreview").click(function (e) {
+
+    modal.style.display = "block";
+    link = this;
+
+    var iframe = document.createElement("iframe");
+    iframe.src
+});
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function (event) {
+    if (event.target == modal) {
         modal.style.display = "none";
-      }
     }
+}
