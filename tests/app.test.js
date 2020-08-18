7f927be7d72c3222a7833786c6d335d68fbfcca8
Mess-Dev <51171387+MesshypeWeb@users.noreply.github.com>
2020-08-18 10:49:32 +0200

    Added more tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,19 +1,26 @@
 const { http } = require('../Server');
 const request = require('supertest');
+var address, os = require('os'), ifaces = os.networkInterfaces();
   beforeAll(() => {
     process.env.NODE_TEST = true;
-  })
-test('get ip', async () => {
-    var address, os = require('os'), ifaces = os.networkInterfaces();
-
+    
     for (var dev in ifaces) {
         var iface = ifaces[dev].filter(function (details) {
             return details.family === 'IPv4' && details.internal === false;
         });
         if (iface.length > 0) address = iface[0].address;
     }
-    const res = await request(http).post('/getip');
+  })
+test('get ip', async () => {
+
+
+    const res = await request(http).get('/getip');
     expect(res.status).toBe(200);
     expect(res.text).toBe(address)
     //expect(res.body).toEqual(address);
 });
+test('presentation is being hosted without errors', async ()=>{
+    const res = await request(http).get('/');
+    expect(res.status).toBe(200)
+    expect(res.header['content-type']).toBe('text/html; charset=UTF-8');
+})
